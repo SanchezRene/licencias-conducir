6107585aceef02c2419681ef85eae7c5feade253
SanchezRene <rene.sanchez6502@gmail.com>
2023-10-31 20:57:03 -0300

    modificación crud solicitudes

--- backend/src/models/solicitud.model.js
+++ backend/src/models/solicitud.model.js
@@ -1,11 +1,24 @@
 "use strict";
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const solicitudSchema = new mongoose.Schema(
+const solicitudSchema = new mongoose.Schema({
 
-    //  desarrollar ...
-
-);
+  userId: {
+    type: Schema.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  fechaCreacion: {
+    type: Date,
+    default: Date.now(),
+  },
+  estado: { 
+    type: String,
+    enum: ["pendiente", "aprobada", "rechazada", "fuera de plazo"],
+    default: "pendiente",
+  },
+});
 
 const Solicitud = mongoose.model("Solicitud", solicitudSchema);
 
